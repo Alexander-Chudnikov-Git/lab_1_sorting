97de7749921486d191da65d3869f99f3bb94c168
RLS-Alexander-Chudnikov <admin@redline-software.xyz>
2023-02-23 14:04:42 +0300

    Initial commit

---
+++ build/x64/CMakeFiles/lab_1_sorting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_1_sorting.dir/main.cpp.o"
+  "lab_1_sorting"
+  "lab_1_sorting.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_1_sorting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/sorting/sorting.hpp
@@ -0,0 +1,37 @@
+/**
+ * @file       <sorting.hpp>
+ * @brief      This header file holds implementation of Sorting class. 
+ * 
+ *             This calss implements sorting algorithms needed for successfull completion of laboratory work 1.
+ *
+ * @author     Alexander Chudnikov (THE_CHOODICK)
+ * @date       15-02-2023
+ * @version    0.0.1
+ * 
+ * @warning    This library is under development, so there might be some bugs in it.  
+ * @bug        Currently, there are no any known bugs. 
+ * 
+ *             In order to submit new ones, please contact me via admin@redline-software.xyz.
+ * 
+ * @copyright  Copyright 2023 Alexander. All rights reserved. 
+ * 
+ *             (Not really)
+ * 
+ * @license    This project is released under the GNUv3 Public License.
+ */
+#ifndef SORTING_HPP
+#define SORTING_HPP
+
+#ifndef MODEL_HPP
+#include "../model/model.hpp"
+#endif // MODEL_HPP
+
+class Sorting
+{
+public:
+	Sorting();
+	~Sorting();
+
+};
+
+#endif // SORTING_HPP
