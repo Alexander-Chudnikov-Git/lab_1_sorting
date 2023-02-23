4d95a388bd47d1ac2b0cd7ab07b3572aa064ab41
RLS-Alexander-Chudnikov <admin@redline-software.xyz>
2023-02-23 14:20:35 +0300

    Revert " a"
    This reverts commit f80e6a9b67756f31d7930a8b12c30d25170c9dbc.

---
+++ src/sorting/sorting.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file       <sorting.cpp>
+ * @brief      This source file holds implementation of Sorting class. 
+ * 
+ *             This calss implements sorting algorithms needed for successfull completion of laboratory work 1.
+ *
+ * @author     Alexander Chudnikov (THE_CHOODICK)
+ * @date       15-02-2023
+ * @version    0.0.1
+ * 
+ * @warning    This library is under development, so there might be some bugs in it.  
+ * @bug        Currently, there are no any known bugs. 
+ * 
+ *             In order to submit new ones, please contact me via admin@redline-software.xyz.
+ * 
+ * @copyright  Copyright 2023 Alexander. All rights reserved. 
+ * 
+ *             (Not really)
+ * 
+ * @license    This project is released under the GNUv3 Public License.
+ */
+#include "sorting.hpp"
+
+Sorting::Sorting()
+{
+
+}
+
+Sorting::~Sorting()
+{
+
+}
